9948577bcaf91ca85118b9c502f06fb9f962f0aa
Michael Gamlem III <mgamlem3@gmail.com>
2022-12-29 19:04:50 -0800

    Add functionality to string utility

--- Mg3.Utility.Tests/StringUtilityTests.cs
+++ Mg3.Utility.Tests/StringUtilityTests.cs
@@ -1,4 +1,5 @@
 using Mg3.Utility.StringUtility;
+using System.Collections.ObjectModel;
 
 namespace Mg3.Utility.Tests.StringUtility;
 public sealed class StringUtilityTests
@@ -25,4 +26,83 @@ public sealed class StringUtilityTests
 	[InlineData(" ")]
 	[InlineData("	")]
 	public void ContainsWhitespace(string s) => Assert.Equal(s?.Any(char.IsWhiteSpace), s?.ContainsWhitespace());
+
+	[Theory]
+	[MemberData(nameof(CharacterListTestData))]
+	public void ContainsCharacterFromCollection(string s, ReadOnlyCollection<char> collection, bool ignoreCase = false)
+	{
+		if (ignoreCase)
+			Assert.Equal(s.Any(x => collection.Any(c => char.ToLowerInvariant(c) == char.ToLowerInvariant(x))), s.ContainsCharacterFromCollection(collection, true));
+		else
+			Assert.Equal(s.Any(x => collection.Any(c => c == x)), s.ContainsCharacterFromCollection(collection));
+	}
+
+	[Theory]
+	[MemberData(nameof(CharacterListTestData))]
+	public void ContainsOnlyCharactersFromCollection(string s, ReadOnlyCollection<char> collection, bool ignoreCase = false)
+	{
+		if (ignoreCase)
+			Assert.Equal(s.All(x => collection.Any(c => char.ToLowerInvariant(c) == char.ToLowerInvariant(x))), s.ContainsOnlyCharactersFromCollection(collection, true));
+		else
+			Assert.Equal(s.All(x => collection.Any(c => c == x)), s.ContainsOnlyCharactersFromCollection(collection));
+	}
+
+	[Theory]
+	[MemberData(nameof(CharacterCaseTestData))]
+	public void ContainsUpperCase(string s) => Assert.Equal(s.Any(char.IsUpper), s.ContainsUpperCase());
+
+	[Theory]
+	[MemberData(nameof(CharacterCaseTestData))]
+	public void ContainsLowerCase(string s) => Assert.Equal(s.Any(char.IsLower), s.ContainsLowerCase());
+
+	[Theory]
+	[MemberData(nameof(CharacterCaseTestData))]
+	public void ContainsMixedCase(string s) => Assert.Equal(s.ContainsUpperCase() && s.ContainsLowerCase(), s.ContainsMixedCase());
+
+	public static IEnumerable<object[]> CharacterListTestData =>
+	new List<object[]>
+	{
+		new object[] { "", MatchingTestCharacterCollection },
+		new object[] { "test", MatchingTestCharacterCollection },
+		new object[] { "test", NotMatchingTestCharacterCollection },
+		new object[] { "Test", MatchingTestCharacterCollection },
+		new object[] { "Test", NotMatchingTestCharacterCollection },
+		new object[] { "Test", MatchingTestCharacterCollection, true },
+		new object[] { "Test", NotMatchingTestCharacterCollection, true },
+		new object[] { "tesT", MatchingTestCharacterCollection },
+		new object[] { "tesT", NotMatchingTestCharacterCollection },
+		new object[] { "tesT", MatchingTestCharacterCollection, true },
+		new object[] { "tesT", NotMatchingTestCharacterCollection, true },
+		new object[] { "TesT", MatchingTestCharacterCollection },
+		new object[] { "TesT", NotMatchingTestCharacterCollection },
+		new object[] { "TesT", MatchingTestCharacterCollection, true },
+		new object[] { "TesT", NotMatchingTestCharacterCollection, true }
+	};
+
+	public static IEnumerable<object[]> CharacterCaseTestData =>
+	new List<object[]>
+	{
+		new object[] { "" },
+		new object[] { "test" },
+		new object[] { "Test" },
+		new object[] { "tesT" },
+		new object[] { "TesT" }
+	};
+
+	private static ReadOnlyCollection<char> MatchingTestCharacterCollection =>
+	new List<char>
+	{
+		't',
+		'e',
+		's',
+		't'
+	}.AsReadOnly();
+
+	private static ReadOnlyCollection<char> NotMatchingTestCharacterCollection =>
+	new List<char>
+	{
+		'f',
+		'o',
+		'o',
+	}.AsReadOnly();
 }
--- Mg3.Utility/StringUtility.cs
+++ Mg3.Utility/StringUtility.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+
 namespace Mg3.Utility.StringUtility;
 
 public static class StringUtility
@@ -22,5 +24,57 @@ public static class StringUtility
 	/// <param name="s"></param>
 	/// <returns></returns>
 	public static bool ContainsWhitespace(this string s) => s.Any(char.IsWhiteSpace);
+
+
+	/// <summary>
+	/// Checks to see if string contains at least one character from supplied collection
+	/// </summary>
+	/// <param name="s">string to compare</param>
+	/// <param name="characters">characters to search for</param>
+	/// <param name="ignoreCase">allows case insensitive operation</param>
+	/// <returns></returns>
+	public static bool ContainsCharacterFromCollection(this string s, ReadOnlyCollection<char> characters, bool ignoreCase = false)
+	{
+		if (ignoreCase)
+			return s.Any(x => characters.Any(c => char.ToLowerInvariant(c) == char.ToLowerInvariant(x)));
+		else
+			return s.Any(x => characters.Any(c => c == x));
+	}
+
+	/// <summary>
+	/// Checks to see if string contains only characters from supplied collection
+	/// </summary>
+	/// <param name="s">string to compare</param>
+	/// <param name="characters">characters to search for</param>
+	/// <param name="ignoreCase">allows case insensitive operation</param>
+	/// <returns></returns>
+	public static bool ContainsOnlyCharactersFromCollection(this string s, ReadOnlyCollection<char> characters, bool ignoreCase = false)
+	{
+		if (ignoreCase)
+			return s.All(x => characters.Any(c => char.ToLowerInvariant(c) == char.ToLowerInvariant(x)));
+		else
+			return s.All(x => characters.Any(c => c == x));
+	}
+
+	/// <summary>
+	/// Checks to see if string contains upper case characters
+	/// </summary>
+	/// <param name="s"></param>
+	/// <returns></returns>
+	public static bool ContainsUpperCase(this string s) => s.Any(char.IsUpper);
+
+	/// <summary>
+	/// Checks to see if string contains lower case characters
+	/// </summary>
+	/// <param name="s"></param>
+	/// <returns></returns>
+	public static bool ContainsLowerCase(this string s) => s.Any(char.IsLower);
+
+	/// <summary>
+	/// Checks to see if string contains upper and lower case characters
+	/// </summary>
+	/// <param name="s"></param>
+	/// <returns></returns>
+	public static bool ContainsMixedCase(this string s) => s.ContainsUpperCase() && s.ContainsLowerCase();
 }
 
