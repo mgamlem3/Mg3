99b9a4c33a72c028289a059b4af7a5788b5810e3
Michael Gamlem III <mgamlem3@gmail.com>
2023-01-05 15:52:56 -0800

    Update type for null allowing string extensions

--- Mg3.Utility/StringUtility.cs
+++ Mg3.Utility/StringUtility.cs
@@ -9,14 +9,14 @@ public static class StringUtility
 	/// </summary>
 	/// <param name="s"></param>
 	/// <returns></returns>
-	public static bool IsNullOrEmpty(this string s) => string.IsNullOrEmpty(s);
+	public static bool IsNullOrEmpty(this string? s) => string.IsNullOrEmpty(s);
 
 	/// <summary>
 	/// Implements <c>string.IsNullOrWhitespace(...)</c> as extension method.
 	/// </summary>
 	/// <param name="s"></param>
 	/// <returns></returns>
-	public static bool IsNullOrWhitespace(this string s) => string.IsNullOrWhiteSpace(s);
+	public static bool IsNullOrWhitespace(this string? s) => string.IsNullOrWhiteSpace(s);
 
 	/// <summary>
 	/// Checks to see if string contains whitespace
